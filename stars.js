4e1e741c14201ee1070c7081729958acafc57ba3
ErikBoesen <me@erikboesen.com>
2021-03-02 17:39:52 -0500

    Add support for double clicking to open full screen

--- stars.js
+++ stars.js
@@ -1,12 +1,20 @@
 let canvas = document.getElementById('canv');
-let WIDTH = window.innerWidth,
-    HEIGHT = window.innerHeight;
-canvas.width  = WIDTH;
-canvas.height = HEIGHT;
-let ORIGIN_X = WIDTH / 2,
-    ORIGIN_Y = HEIGHT / 2;
-let STAR_SIZE_MULTIPLIER = 0.03;
 let ctx = canvas.getContext('2d');
+let WIDTH, HEIGHT,
+    ORIGIN_X, ORIGIN_Y,
+    STAR_SIZE_MULTIPLIER;
+
+function setConstants() {
+    console.log('Initializing constants');
+    WIDTH = window.innerWidth;
+    HEIGHT = window.innerHeight;
+    canvas.width  = WIDTH;
+    canvas.height = HEIGHT;
+    ORIGIN_X = WIDTH / 2;
+    ORIGIN_Y = HEIGHT / 2;
+    STAR_SIZE_MULTIPLIER = 0.03;
+}
+setConstants();
 
 const NUM_STARS = 40;
 
@@ -16,6 +24,7 @@ let stars = [];
 
 function createStar() {
     var star = {};
+    console.log(ORIGIN_X);
     star.x = Math.random() * WIDTH - ORIGIN_X;
     star.y = Math.random() * HEIGHT - ORIGIN_Y;
     star.z = star.max_depth = Math.max(WIDTH, HEIGHT);
@@ -81,3 +90,38 @@ for (let i = 0; i < NUM_STARS; i++) {
 
 draw();
 let main_loop = setInterval(draw, 10);
+
+function openFullscreen() {
+    console.log('Opening fullscreen');
+    const elem = canvas;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    console.log('Closing fullscreen');
+    const elem = canvas;
+    if (elem.exitFullscreen) {
+        elem.exitFullscreen();
+    } else if (elem.webkitExitFullscreen) { /* Safari */
+        elem.webkitExitFullscreen();
+    } else if (elem.msExitFullscreen) { /* IE11 */
+        elem.msExitFullscreen();
+    }
+}
+
+let fullscreen = false;
+canvas.ondblclick = function() {
+    if (fullscreen) {
+        closeFullscreen();
+    } else {
+        openFullscreen();
+    }
+    fullscreen = !fullscreen;
+    setConstants();
+}
